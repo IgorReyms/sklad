728b48ea27451818849f239ee1de42aca9894f6b
igor <igor8501@mail.ru>
2024-02-05 02:23:19 +0300

    0

--- create_document/convertor.py
+++
@@ -1,8 +0,0 @@
-from docx2pdf import convert
-
-def convert(path_input: str, path_output: str) -> bool:
-    try:
-        convert(path_input, path_output)
-        return True
-    except:
-        return False
